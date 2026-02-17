fa43974f10d1abe6259a2c20fb7ed096a87af6dc
Abdessadek <abdessadekhm@gmail.com>
2026-02-17 11:05:11 +0000

    [UPD] reservation: add compute function to subtotal in reservation.line

--- reservation/models/reservation_reservation_line.py
+++ reservation/models/reservation_reservation_line.py
@@ -1,17 +1,23 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ReservationLine(models.Model):
     _name = "reservation.reservation.line"
     _description="table of reservation lines"
 
-    name=fields.Char(string="Reservation line")
     
-    reservation_id=fields.Many2one("reservation.reservation")
+    reservation_id=fields.Many2one("reservation.reservation", readonly=True)
     product_id=fields.Many2one("product.product")
 
     quantity=fields.Integer(string="Quantity")
     unit_price=fields.Float(string="Price")
-    subtotal=fields.Float(compute="")
+    subtotal=fields.Float(compute="_calculate_subtotal")
+
+
+    @api.depends("quantity", "unit_price")
+    def _calculate_subtotal(self):
+        for record in self:
+            record.subtotal=record.unit_price*record.quantity
+        pass
 
 
 
