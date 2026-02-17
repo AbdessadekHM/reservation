48be8baa5cc16b18447fe11e62ab56b29017553b
Abdessadek <abdessadekhm@gmail.com>
2026-02-17 11:14:50 +0000

    [UPD] reservation: Add compute function to calculate total amount & add inline views for reservation.line model

--- reservation/models/reservation_reservation.py
+++ reservation/models/reservation_reservation.py
@@ -15,34 +15,19 @@ class Reservation(models.Model):
     ], default='draft')
     sale_order_ids=fields.One2many("sale.order", "reservation_id")
     line_ids=fields.One2many("reservation.reservation.line", "reservation_id")
-    amount_total=fields.Float(compute="")
+    amount_total=fields.Float(compute="_calculate_amount_total")
 
-
-    def _create_name(self):
-        for record in self:
-            sequence_number = self.env["ir.sequence"].next_by_code('custom.order')
-            print("\n\n\n")
-            print("\n\n\n")
-            print("sequenced number is ")
-            print(sequence_number)
-            print("\n\n\n")
-            print("\n\n\n")
-            record.name=sequence_number
-        pass
     def _get_sequence_number(self):
 
         sequence_number = self.env["ir.sequence"].next_by_code('reservation.reservation')
 
-        print("\n\n\n")
-        print("\n\n\n")
-        print("sequenced number is ")
-        print(sequence_number)
-        print("\n\n\n")
-        print("\n\n\n")
         return sequence_number
 
+    @api.depends("line_ids")
+    def _calculate_amount_total(self):
+        for record in self:
+            record.amount_total=sum([line.subtotal for line in record.line_ids])
 
-        pass
-
+    
 
 
