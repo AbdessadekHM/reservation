8cc6d0650e00eed3907019c376d33545f76c6d6c
Abdessadek <abdessadekhm@gmail.com>
2026-02-17 09:19:56 +0000

    [ADD] reservation: Create reservation module

---
+++ reservation/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "reservation_management",
+    "version" : "1.0",
+    "author": "Abdessadek Hmiddouch",
+    "depends": ["base", "mail", "sale", "portal", "website"],
+    "category": "Reservation",
+    "description": "Reservation Module"
+}
