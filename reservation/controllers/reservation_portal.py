22026560e4026ed81d5a523a01815c8851dbde11
Abdessadek <abdessadekhm@gmail.com>
2026-02-19 10:21:59 +0000

    [ADD] reservation: add download feature to reservation detail's portal

--- reservation/controllers/reservation_portal.py
+++ reservation/controllers/reservation_portal.py
@@ -54,5 +54,26 @@ class ReservationPortal(CustomerPortal):
         })
 
         pass
+    @http.route(["/my/reservations/pdf/<id>"], type="http")
+    def get_pdf(self, id):
+        reservation = request.env["reservation.reservation"].search([
+            ('id', '=', id)
+        ])
+
+        pdf, _ = request.env["ir.actions.report"]._render_qweb_pdf(
+            "reservation.action_report_reservation", [reservation.id]
+        )
+
+        http_headers = {
+            ('Content-Type', 'application/pdf'),
+            ('Content-Length', len(pdf)),
+            ('Content-Disposition', f'attachement; filename="Reservation_{reservation.name}.pdf"')
+        }
+
+        return request.make_response(pdf,headers=http_headers)
+
+
+
+        pass
 
     pass
