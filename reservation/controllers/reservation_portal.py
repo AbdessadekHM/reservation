32843c0d507ad774f859f628def9497fcd0082d8
Abdessadek <abdessadekhm@gmail.com>
2026-02-23 09:06:43 +0000

    [ADD] reservation: Add the sorting bar to reservation's portal

--- reservation/controllers/reservation_portal.py
+++ reservation/controllers/reservation_portal.py
@@ -1,19 +1,52 @@
-from odoo import http
+from odoo import http, _
 from odoo.addons.portal.controllers.portal import CustomerPortal
 from odoo.http import request #type:ignore
 
 
 class ReservationPortal(CustomerPortal):
+    def _prepare_searchbar_sortings(self):
+        return {
+            'date': {'label': _('Newest'), 'order': 'create_date desc'},
+            'name': {'label': _('Name'), 'order': 'name'},
+        }
 
     @http.route(["/my/reservations"], type="http", auth="user", website=True)
-    def get_reservations(self):
+    def get_reservations(self, search=None, filterby=None,sortby=None, search_in='content'):
         values = self._prepare_portal_layout_values()
-        reservations = request.env["reservation.reservation"].search([
-            ('create_uid','=',request.env.user.id)
-        ])
+
+        filters = {
+            'all': {'label': 'All', 'domain': [('create_uid', '=',request.env.user.id)]},
+            'draft': {'label': 'Draft', 'domain': [('state', '=', 'draft')]},
+            'done': {'label': 'Done', 'domain': [('state', '=', 'done')]},
+        }
+
+        searchbar_sortings = self._prepare_searchbar_sortings()
+
+        if not sortby:
+            sortby = 'name'
+        order = searchbar_sortings[sortby]['order']
+
+
+
+        if not filterby:
+            filterby = 'all'
+        domain = filters.get(filterby, filters['all'])['domain']
+
+        if search:
+            domain += [('name', 'ilike', search)]
+
+        print("\n\n\n\n\n")
+        print(domain)
+        print("\n\n\n\n\n")
+
+        reservations = request.env["reservation.reservation"].search(domain,order=order)
         values.update({
             'reservations': reservations,
             'page_name': 'my reservations',
+            'search': search,
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
+            'search_in': search_in
         })
 
         return request.render("reservation.portal_my_reservations", values)
